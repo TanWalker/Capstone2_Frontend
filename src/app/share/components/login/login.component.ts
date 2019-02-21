faa3122a305a1bc6383c1223daf008a7282b12cf
eddy enclave <eddy@enclave.vn>
2019-02-21 14:54:44 +0700

    user service

--- src/app/share/components/login/login.component.ts
+++ src/app/share/components/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Login } from '../../models/login';
+import { UserService } from '../../services/user.service';
+import { Result } from '../../models/result';
 
 @Component({
   selector: 'app-login',
@@ -8,14 +11,32 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
+  public loginInfo: Login = new Login();
   constructor(
-    private router: Router
+    private router: Router,
+    private userService: UserService
   ) { }
 
   ngOnInit() {
   }
 
   login() {
+
+    // post login model
+
+    // step 1 : set value for login
+    this.loginInfo.username = 'aaaaaa';
+    this.loginInfo.password = '12312';
+
+
+    this.userService.login(this.loginInfo).subscribe(
+      (response: Result) => {
+        console.log(response);
+
+        // token
+      }
+    );
+
     this.router.navigate(['/class']);
   }
 }
---
+++ src/app/share/services/user.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Login } from '../models/login';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+constructor(
+  private http: HttpClient
+) { }
+
+login(loginInfo: Login) {
+  return this.http.post(`${environment.urls.api}` + `/login`, loginInfo);
+}
+}
