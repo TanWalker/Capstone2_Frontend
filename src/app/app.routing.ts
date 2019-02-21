0862a096daed24a7a38742bafcd7cbf6955d6ddb
Ocean <ocean@enclave.vn>
2019-02-21 15:44:23 +0700

    update register page design

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,17 +1,25 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './share/components/login/login.component';
+import { RegisterComponent } from './share/components/register/register.component';
 
 const routes: Routes = [
   {
-    path: '', redirectTo: 'login', pathMatch: 'full'
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
   },
   {
-     path: 'login', component: LoginComponent
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
   }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutes { }
+export class AppRoutes {}
