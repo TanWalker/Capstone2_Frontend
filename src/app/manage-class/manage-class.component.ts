32d9b7e513c06c64bbeaa26e6570524016a3c2d1
eddy enclave <eddy@enclave.vn>
2019-02-22 15:27:41 +0700

    check isMobile

--- src/app/manage-class/manage-class.component.ts
+++ src/app/manage-class/manage-class.component.ts
@@ -9,23 +9,13 @@ import { DeviceDetectorService } from 'ngx-device-detector';
 })
 export class ManageClassComponent implements OnInit {
 
-  deviceInfo = null;
+  isMobile = false;
   constructor(
     private deviceService: DeviceDetectorService,
   ) {
-    // this.epicFunction();
+   this.isMobile = this.deviceService.isMobile();
   }
-  epicFunction() {
-    console.log('hello `Home` component');
-    this.deviceService.isMobile();
-    const isMobile = this.deviceService.isMobile();
-    const isTablet = this.deviceService.isTablet();
-    const isDesktopDevice = this.deviceService.isDesktop();
-    console.log(this.deviceInfo);
-    console.log(isMobile);  // returns if the device is a mobile device (android / iPhone / windows-phone etc)
-    console.log(isTablet);  // returns if the device us a tablet (iPad etc)
-    console.log(isDesktopDevice); // returns if the app is running on a Desktop browser.
-  }
+
   ngOnInit() {
   }
 }
