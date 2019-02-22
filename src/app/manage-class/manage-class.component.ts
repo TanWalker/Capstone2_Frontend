d265055c27604939f7d32b8abee807838303f2d8
Ocean <ocean@enclave.vn>
2019-02-22 16:57:44 +0700

    update device detector

--- src/app/manage-class/manage-class.component.ts
+++ src/app/manage-class/manage-class.component.ts
@@ -9,23 +9,12 @@ import { DeviceDetectorService } from 'ngx-device-detector';
 })
 export class ManageClassComponent implements OnInit {
 
-  deviceInfo = null;
+  isMobile = null;
   constructor(
     private deviceService: DeviceDetectorService,
   ) {
-    // this.epicFunction();
-  }
-  epicFunction() {
-    console.log('hello `Home` component');
-    this.deviceService.isMobile();
-    const isMobile = this.deviceService.isMobile();
-    const isTablet = this.deviceService.isTablet();
-    const isDesktopDevice = this.deviceService.isDesktop();
-    console.log(this.deviceInfo);
-    console.log(isMobile);  // returns if the device is a mobile device (android / iPhone / windows-phone etc)
-    console.log(isTablet);  // returns if the device us a tablet (iPad etc)
-    console.log(isDesktopDevice); // returns if the app is running on a Desktop browser.
   }
   ngOnInit() {
+    this.isMobile = this.deviceService.isMobile();
   }
 }
