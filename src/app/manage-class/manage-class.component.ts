95f6463ab56034c597d02667c3853057adb8b051
Ocean <ocean@enclave.vn>
2019-02-24 21:04:10 +0700

    update desktop view menu link

--- src/app/manage-class/manage-class.component.ts
+++ src/app/manage-class/manage-class.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DeviceDetectorService } from 'ngx-device-detector';
 import { AuthService } from '../share/services/auth.service';
+import { ActivatedRoute, Router , NavigationEnd } from '@angular/router';
 
 
 @Component({
@@ -11,12 +12,29 @@ import { AuthService } from '../share/services/auth.service';
 export class ManageClassComponent implements OnInit {
 
   isMobile = null;
+  currentUrl: string;
   constructor(
     private deviceService: DeviceDetectorService,
+    private ActiveRoute: ActivatedRoute,
+    private router: Router
   ) {
+    router.events.subscribe((_: NavigationEnd) => {
+      // example: NavigationStart, RoutesRecognized, NavigationEnd
+      if (_.url !== undefined) {
+        this.currentUrl = _.url;
+      }
+      // console.log(this.ActiveRoute.snapshot.paramMap.get('id'));
+    });
   }
 
   ngOnInit() {
     this.isMobile = this.deviceService.isMobile();
+    this.currentUrl = this.router.url;
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentUrl = event.url;
+        // console.log(this.currentUrl);
+      }
+    });
   }
 }
