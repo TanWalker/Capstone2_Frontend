8816c92840d694bbddafd3b6c1e6839eca591b6b
Ocean <ocean@enclave.vn>
2019-02-24 21:47:46 +0700

    fix login, register width view conflict with inside view

--- src/app/manage-class/manage-class.component.ts
+++ src/app/manage-class/manage-class.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DeviceDetectorService } from 'ngx-device-detector';
 import { AuthService } from '../share/services/auth.service';
-import { ActivatedRoute, Router , NavigationEnd } from '@angular/router';
 
 
 @Component({
@@ -15,26 +14,11 @@ export class ManageClassComponent implements OnInit {
   currentUrl: string;
   constructor(
     private deviceService: DeviceDetectorService,
-    private ActiveRoute: ActivatedRoute,
-    private router: Router
   ) {
-    router.events.subscribe((_: NavigationEnd) => {
-      // example: NavigationStart, RoutesRecognized, NavigationEnd
-      if (_.url !== undefined) {
-        this.currentUrl = _.url;
-      }
-      // console.log(this.ActiveRoute.snapshot.paramMap.get('id'));
-    });
+
   }
 
   ngOnInit() {
     this.isMobile = this.deviceService.isMobile();
-    this.currentUrl = this.router.url;
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        this.currentUrl = event.url;
-        // console.log(this.currentUrl);
-      }
-    });
   }
 }
--- src/app/share/components/login/login.component.ts
+++ src/app/share/components/login/login.component.ts
@@ -9,7 +9,7 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css','../../../app.component.css']
 })
 export class LoginComponent implements OnInit {
 
