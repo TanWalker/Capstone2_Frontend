d382e9f5d54ea313ce16ae78e33e84b085336e5a
Ocean <ocean@enclave.vn>
2019-03-01 16:30:56 +0700

    Update multi-level menu sidenav UI

--- src/app/manage-class/manage-class.component.ts
+++ src/app/manage-class/manage-class.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DeviceDetectorService } from 'ngx-device-detector';
 import { AuthService } from '../share/services/auth.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-manage-class',
@@ -8,13 +9,32 @@ import { AuthService } from '../share/services/auth.service';
   styleUrls: ['./manage-class.component.css', './../app.component.css']
 })
 export class ManageClassComponent implements OnInit {
+  currentUrl: string;
   isMobile = null;
   user = this.authService.currentUser;
   constructor(
     private deviceService: DeviceDetectorService,
-    private authService: AuthService
-    ) {}
+    private authService: AuthService,
+    private router: Router
+  ) {
+    router.events.subscribe((_: NavigationEnd) => {
+      // example: NavigationStart, RoutesRecognized, NavigationEnd
+      if (_.url !== undefined) {
+        this.currentUrl = _.url;
+      }
+      // console.log(this.ActiveRoute.snapshot.paramMap.get('id'));
+    });
+  }
+
   ngOnInit() {
+    this.currentUrl = this.router.url;
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.currentUrl = event.url;
+        console.log(this.currentUrl);
+      }
+    });
+
     this.isMobile = this.deviceService.isMobile();
     console.log(this.user);
   }
