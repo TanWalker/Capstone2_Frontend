4ef26b17a937438578dc4fd2bc19ce07d6b77f50
Ocean <ocean@enclave.vn>
2019-03-14 13:21:50 +0700

    update edit schedule form

--- src/app/manage-class/exercises/dialogs/detail-schedule/detail-schedule.component.ts
+++ src/app/manage-class/exercises/dialogs/detail-schedule/detail-schedule.component.ts
@@ -4,21 +4,31 @@ import { NgbDateStruct, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
 import { Result } from 'src/app/share/models/result';
 import { TeamService } from 'src/app/share/services/team.service';
 import { Class } from 'src/app/share/models/class';
+import { ExerciseService } from 'src/app/share/services/exercise.service';
+import { Exercise } from 'src/app/share/models/exercise';
 
 @Component({
   selector: 'app-detail-schedule',
   templateUrl: './detail-schedule.component.html',
-  styleUrls: ['./detail-schedule.component.css']
+  styleUrls: [
+    './detail-schedule.component.css',
+    '../../../../app.component.css',
+    '../../../manage-class.component.css'
+  ]
 })
 export class DetailScheduleComponent implements OnInit {
   public dateModel: NgbDateStruct;
-  public teams: Class [] = [];
+  public teams: Class[] = [];
+  public exercises: Exercise[] = [];
+  public currentTeam;
+  public currentExercise;
   constructor(
     private dialogRef: MatDialogRef<DetailScheduleComponent>,
     @Inject(MAT_DIALOG_DATA) private data: any,
     private dialog: MatDialog,
     private calendar: NgbCalendar,
-    private teamService: TeamService
+    private teamService: TeamService,
+    private exerciseService: ExerciseService
   ) {
     console.log(data.schedule);
   }
@@ -26,13 +36,21 @@ export class DetailScheduleComponent implements OnInit {
   ngOnInit() {
     this.dateModel = this.calendar.getToday();
     this.getTeams();
+    this.getExercises();
+    console.log(this.data);
+    this.currentTeam = this.data.schedule.title;
+    this.currentExercise = this.data.schedule.id;
   }
   getTeams() {
-    this.teamService.getTeamByCoach().subscribe(
-      (data: Result) => {
-        console.log(data);
-        this.teams = data.success ? data.values : [];
-      }
-    );
+    this.teamService.getTeamByCoach().subscribe((data: Result) => {
+      // console.log(data);
+      this.teams = data.success ? data.values : [];
+    });
+  }
+  getExercises() {
+    this.exerciseService.getExerciseByCoach().subscribe((data: Result) => {
+      // console.log(data);
+      this.exercises = data.success ? data.values : [];
+    });
   }
 }
