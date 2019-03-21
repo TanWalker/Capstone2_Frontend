e11edba1fb971843f4b20dcffe2aff52e231cbdf
Ocean <ocean@enclave.vn>
2019-03-21 17:07:11 +0700

    updating record

--- src/app/manage-class/record/record.component.ts
+++ src/app/manage-class/record/record.component.ts
@@ -8,6 +8,7 @@ import { Class } from 'src/app/share/models/class';
 import { Exercise } from 'src/app/share/models/exercise';
 import { Record } from 'src/app/share/models/record';
 import { RecordService } from 'src/app/share/services/record.service';
+import { LessonService } from 'src/app/share/services/lesson.service';
 
 @Component({
   selector: 'app-record',
@@ -19,7 +20,6 @@ import { RecordService } from 'src/app/share/services/record.service';
   ]
 })
 export class RecordComponent implements OnInit, OnDestroy {
-
   public isMobile = false;
   public subSchedule: any;
   public schedules: Schedule[] = [];
@@ -28,49 +28,57 @@ export class RecordComponent implements OnInit, OnDestroy {
   public currentExercise: Exercise = new Exercise();
   public subDefaultSchedule: any;
   constructor(
-    public deviceService: DeviceDetectorService,
+    private deviceService: DeviceDetectorService,
     private scheduleService: ScheduleService,
+    private lessonService: LessonService,
     private recordService: RecordService
   ) {
     this.isMobile = deviceService.isMobile();
-   }
+  }
 
   ngOnInit() {
-    // this.getListSchedule();
+    this.getListSchedule();
     // this.getDefaultSchedule();
+    this.testAddRecord();
+    this.getListLesson();
   }
   ngOnDestroy() {
-    if ( this.subSchedule !== null) {this.subSchedule.unsubscribe(); }
+    if (this.subSchedule !== null) {
+      this.subSchedule.unsubscribe();
+    }
   }
   getListSchedule() {
-    this.subSchedule = this.scheduleService.getScheduleByCoach().subscribe(
-      (data: Result) => {
+    this.subSchedule = this.scheduleService
+      .getScheduleByCoach()
+      .subscribe((data: Result) => {
         this.schedules = data.success ? data.values : [];
-        this.schedules = _.orderBy(this.schedules,  ['time_start'], ['desc']);
-        if ( this.schedules !== []) {
+        this.schedules = _.orderBy(this.schedules, ['time_start'], ['desc']);
+        if (this.schedules !== []) {
           this.currentSchedule = this.schedules[0];
           this.currentTeam.id = this.schedules[0].team_id;
           this.currentTeam.name = this.schedules[0].team_name;
         }
-      }
-    );
+      });
+  }
+  getListLesson() {
+    this.lessonService.getLessonByCoach().subscribe((data: Result) => {
+      console.log(data.values);
+    });
   }
   onChangeSchedule(schedule: Schedule) {
-
+    console.log(schedule);
   }
   getDefaultSchedule() {
-    this.subDefaultSchedule = this.scheduleService.getDefaultScheduleByCurrentDate().subscribe(
-      (data: Result) => {
+    this.subDefaultSchedule = this.scheduleService
+      .getDefaultScheduleByCurrentDate()
+      .subscribe((data: Result) => {
         console.log(data);
-      }
-    );
+      });
   }
-  addRecord( record: Record ) {
-      this.recordService.addRecord(record).subscribe(
-        (res: Result) => {
-          console.log(res);
-        }
-      );
+  addRecord(record: Record) {
+    this.recordService.addRecord(record).subscribe((res: Result) => {
+      console.log(res);
+    });
   }
   testAddRecord() {
     const record = new Record();
@@ -80,7 +88,7 @@ export class RecordComponent implements OnInit, OnDestroy {
     record.max_time = 15.6;
     record.heart_rate = 65;
     record.exercise_id = '14';
-    record.schedule_id = '2';
+    record.schedule_id = '1';
     record.user_id = '10';
     record.time_swim = 14;
     record.errors = 'thực hiện lỗi';
