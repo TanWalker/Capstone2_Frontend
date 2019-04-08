977a5bcd9e45e68fd2c1f2ee35bad1b8361f1946
Ocean <ocean@enclave.vn>
2019-04-08 17:23:28 +0700

    update trainee record chart function

--- src/app/trainee/stats/record-by-month-year/record-by-month-year.component.ts
+++ src/app/trainee/stats/record-by-month-year/record-by-month-year.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Color, Label } from 'ng2-charts';
 import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
 import { RecordService } from 'src/app/share/services/record.service';
 import { Result } from 'src/app/share/models/result';
 import { Record } from 'src/app/share/models/record';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-record-by-month-year',
@@ -12,62 +13,26 @@ import { Record } from 'src/app/share/models/record';
   styleUrls: ['./record-by-month-year.component.css']
 })
 export class RecordByMonthYearComponent implements OnInit {
-  public barChartLabels: Label[] = [
-    'T1',
-    'T2',
-    'T3',
-    'T4',
-    'T6',
-    'T6',
-    'T7',
-    'T8',
-    'T9',
-    'T10',
-    'T11',
-    'T12'
-  ];
+  public times = [];
+  public heart_rate = [];
+  public barChartLabels: Label[] = [];
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
 
   public barChartData: ChartDataSets[] = [
     {
-      data: [
-        this.randomNum(),
-        this.randomNum(),
-        this.randomNum(),
-        this.randomNum(),
-        this.randomNum(),
-        this.randomNum(),
-        this.randomNum(),
-        this.randomNum(),
-        this.randomNum(),
-        this.randomNum(),
-        this.randomNum(),
-        this.randomNum()
-      ],
+      data: this.times,
       label: 'Thời gian'
     },
     {
-      data: [
-        this.randomNum(),
-        this.randomNum(),
-        this.randomNum(),
-        this.randomNum(),
-        this.randomNum(),
-        this.randomNum(),
-        this.randomNum(),
-        this.randomNum(),
-        this.randomNum(),
-        this.randomNum(),
-        this.randomNum(),
-        this.randomNum()
-      ],
+      data: this.heart_rate,
       label: 'Nhịp tim',
       yAxisID: 'y-axis-1'
     }
   ];
   public barChartOptions: ChartOptions & { annotation: any } = {
-    responsive: true,
+    // responsive: true,
+    // maintainAspectRatio: false,
     scales: {
       // We use this empty structure as a placeholder for dynamic theming.
       xAxes: [{}],
@@ -130,6 +95,9 @@ export class RecordByMonthYearComponent implements OnInit {
   public year: Number;
   public exercise_id: Number;
   public records: Record[];
+  public teststyle: NgStyle;
+  public divStyle;
+  public widthChart;
   constructor(
     private activatedRoute: ActivatedRoute,
     private recordService: RecordService
@@ -143,14 +111,26 @@ export class RecordByMonthYearComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.divStyle = 'none';
     this.recordService
       .getListRecordByMonthOfYear(this.month, this.year, this.exercise_id)
       .subscribe((data: Result) => {
         if (data.success) {
-          console.log(data);
+          // console.log(data);
           this.records = data.values;
+          console.log(this.records.length);
+          this.records.forEach(record => {
+            this.barChartLabels.push('Ngày ' + record.schedule.day.toString());
+            this.times.push(record.time_swim);
+            this.heart_rate.push(record.heart_rate);
+          });
+          this.divStyle = 'block';
+          setTimeout(function() {
+            this.widthChart = '100';
+            console.log('hello');
+          }, 100);
+          // this.widthChart = '100';
         }
-        console.log(data);
       });
   }
   public randomNum() {
