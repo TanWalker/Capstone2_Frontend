cd1a478254e7af42642fb80dfe4920a2989eb1be
Ocean <ocean@enclave.vn>
2019-04-26 15:57:50 +0700

    update miband link

--- src/app/trainee/trainee-schedule/dialog/schedule-info/schedule-info.component.ts
+++ src/app/trainee/trainee-schedule/dialog/schedule-info/schedule-info.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { ExerciseService } from 'src/app/share/services/exercise.service';
 import { Result } from 'src/app/share/models/result';
-
+import { AuthService } from 'src/app/share/services/auth.service';
+import { environment } from 'src/environments/environment';
+import { User } from 'src/app/share/models/user';
 @Component({
   selector: 'app-schedule-info',
   templateUrl: './schedule-info.component.html',
@@ -11,16 +13,21 @@ import { Result } from 'src/app/share/models/result';
 export class ScheduleInfoComponent implements OnInit {
   public schedule;
   public exercises;
+  public apiUrl;
+  public user = new User();
   constructor(
     private dialogRef: MatDialogRef<ScheduleInfoComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private exerciseService: ExerciseService
+    private exerciseService: ExerciseService,
+    private authService: AuthService
   ) {}
 
   ngOnInit() {
     this.schedule = this.data.schedule.meta;
     console.log(this.schedule);
     console.log(('0' + 11).slice(-2));
+    this.user = this.authService.currentUser;
+    this.apiUrl = environment.urls.api;
     this.exerciseService
       .getExerciseByLessonID(this.schedule.lesson_id)
       .subscribe((result: Result) => {
